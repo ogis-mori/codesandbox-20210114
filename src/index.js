3cd5d276b1ddedb4c0354bcb8d0b0396bc8a0760
ogis-mori <mori_miki@ogis-ri.co.jp>
2021-01-15 10:32:51 +0900

    step6

--- src/index.js
+++ src/index.js
@@ -4,7 +4,40 @@ const onClickAdd = () => {
   //テキストボックスの値を取得し、初期化する
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
-  alert(inputText);
+  //alert(inputText);
+
+  //div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+  //console.log(div);
+
+  //liタグ生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+  //console.log(li);
+
+  //button（完了）タグ生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  //button（削除）タグ生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    alert("削除");
+  });
+
+  //divタグの子要素に各要素を設定
+  div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+  //console.log(div);
+
+  //未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(div);
 };
 
 document
